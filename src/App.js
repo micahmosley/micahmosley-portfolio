f1d7ca32d137d7f7714c411092fe6f3dab4b6d57
micahmosley <micah.mosley@utexas.edu>
2020-11-11 15:44:10 -0600

    created navigation and home components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Home from './Components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      Micah Mosley is the best
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Navigation setLoadingScreen={this.setLoadingScreen} userCreateOrLogin={this.userCreateOrLogin} getPlacedBets={this.getPlacedBets} username={this.state.username} balance={this.state.balance} logout={this.logout} userId={this.state.userId} />
+          <Switch>
+            <div className="main">
+            <Route exact path="/" component={Home} />
+              <Route exact path="/account" render={(props) => (
+                this.state.userId !== 0 ? <Account logout={this.logout} passwordChange={this.state.passwordChange} editPassword={this.editPassword} balance={this.state.balance} username={this.state.username} /> :
+                  <Home error={this.state.error} showError={this.state.showError} hideError={this.hideError} />
+              )} />
+            </div>
+          </Switch>
+        </Router>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Nav, Navbar, NavDropdown, Modal } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+class Navigation extends React.Component {
+
+
+    render() {
+      return (
+        
+      )
+    }
+  }
+  
+export default Navigation
